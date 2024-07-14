2f6a8acf01287fdef6b9a81ba7eabeff00adc360
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-07-14 15:33:10 +0400

    react initial commit

---
+++ js_nepomnyashiy/script.js
@@ -0,0 +1,73 @@
+// const queryValue = `
+// query getTodos {
+//   todos{
+//     data{
+//       id
+//       title
+//     }
+//   }
+// }
+// `
+//
+// fetch("https://graphqlzero.almansi.me/api",
+//     {
+//         method: "POST",
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({query: queryValue}),
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data.data.todos.data))
+
+// const queryValue = `
+// mutation changeStatus ($id:ID!, $input: UpdateTodoInput!) {
+//   updateTodo (id:$id, input:$input){
+//   title
+//   }
+// }
+// `
+// const variablesValue = {
+//     id: 1,
+//     input: {title: "new title"}
+//
+// }
+//
+//
+// fetch("https://graphqlzero.almansi.me/api",
+//     {
+//         method: "POST",
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//             query: queryValue,
+//             variables: variablesValue
+//         }),
+//
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data.data.updateTodo.title))
+
+
+// const queryValue = `
+// mutation addTodo ($input:CreateTodoInput!){
+//   createTodo (input:$input){
+//     id
+//   }
+// }
+// `
+// const variablesValue = {
+//     "input": {"title": "new todo title", "completed": false}
+// }
+//
+//
+// fetch("https://graphqlzero.almansi.me/api",
+//     {
+//         method: "POST",
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//             query: queryValue,
+//             variables: variablesValue
+//         }),
+//
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
---
+++ react_neponyashiy/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            ura
+        </div>
+    );
+}
+
+export default App;

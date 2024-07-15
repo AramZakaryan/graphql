a4c771d9994f42ad0e094e2b1cd8e6625bd82c28
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-07-15 22:18:38 +0400

    graphql mutation-delete -update

--- react_nepomnyashiy/src/App.tsx
+++ react_nepomnyashiy/src/App.tsx
@@ -96,7 +96,7 @@
 // export default App;
 
 
-import { ApolloCache, gql, useMutation, useQuery } from "@apollo/client"
+import { gql, useMutation, useQuery } from "@apollo/client"
 
 function App() {
 
@@ -115,15 +115,15 @@ function App() {
     = useQuery<GetTodosData>(GET_TODOS)
 
   const ADD_TODO = gql`
-    mutation addTodo ($title: String!, $user_id: ID!, $completed: Boolean!){
-      createTodo(title:$title, user_id:$user_id, completed:$completed){
-        id
-        title
-        user_id
-        completed
+      mutation addTodo ($title: String!, $user_id: ID!, $completed: Boolean!){
+          createTodo(title:$title, user_id:$user_id, completed:$completed){
+              id
+              title
+              user_id
+              completed
+          }
       }
-    }
-    `
+  `
 
   const ADD_TODO_VARIABLES = {
     "title": "new todo title",
@@ -131,8 +131,7 @@ function App() {
     "completed": false
   }
 
-  const [addTodo]
-    = useMutation<AddTodoData>(ADD_TODO, {
+  const [addTodo] = useMutation<AddTodoData>(ADD_TODO, {
     // refetchQueries: [GET_TODOS]
     update(cache, data) {
 
@@ -141,8 +140,6 @@ function App() {
       })
 
       if (!cachedTodos) return
-
-      console.log(cache)
 
       cache.writeQuery({
         query: GET_TODOS,
@@ -160,6 +157,72 @@ function App() {
     addTodo({ variables: ADD_TODO_VARIABLES })
   }
 
+  const DELETE_TODO = gql`
+      mutation deleteTodo ($id: ID!){
+        removeTodo(id:$id){
+          id
+        }
+      }
+  `
+
+  const [deleteTodo] = useMutation<DeleteTodoData>(DELETE_TODO,
+    {
+      update(cache, data) {
+        cache.modify<{ allTodos: Array<{ __ref: string }> }>({
+            fields: {
+              allTodos(currentAllTodos) {
+                return currentAllTodos?.filter((todo) => todo.__ref !== `Todo:${data.data?.removeTodo.id}`)
+              }
+            }
+          }
+        )
+
+      }
+    }
+  )
+
+  const deleteTodoHandler = (id: number) => {
+    deleteTodo({
+      variables: {
+        id
+      }
+    })
+  }
+
+  const CHANGE_TODO_STATUS = gql`
+    mutation changeTodoStatus ($id: ID!,$completed: Boolean){
+      updateTodo (id:$id, completed: $completed) {
+      id
+      completed
+      }
+    }
+  `
+
+  const [changeTodoStatus] = useMutation<ChangeTodoStatusData>(
+    CHANGE_TODO_STATUS
+    // ,
+    // {
+    // update(cache, data) {
+    //   const cachedTodos = cache.readQuery<{ allTodos: Todo[] }>({ query: GET_TODOS })
+    //   if (!cachedTodos) return
+    //   cache.writeQuery({
+    //     query: GET_TODOS,
+    //     data: {
+    //       allTodos: cachedTodos.allTodos.filter(todo => todo.id !== data.data?.removeTodo.id)
+    //     }
+    //   })
+    // }
+  )
+
+  const changeTodoStatusHandler = (id: number, completed: boolean) => {
+    changeTodoStatus({
+      variables: {
+        id,
+        completed
+      }
+    })
+  }
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error : {error.message}</p>
 
@@ -168,7 +231,13 @@ function App() {
       <button onClick={addTodoHandler}>add new todo
       </button>
       <ul>
-        {data?.allTodos.map((t) => <li key={t.id}>{t.id} - {t.title} - {t.user_id} - {t.completed.toString()}</li>)}
+        {data?.allTodos.map((t) =>
+          <li key={t.id}>{t.id} - {t.title} - {t.user_id} - <b
+            onClick={() => changeTodoStatusHandler(t.id, !t.completed)}>{t.completed.toString()}</b>
+            <button style={{ marginLeft: "10px" }} onClick={() => deleteTodoHandler(t.id)}>X
+            </button>
+          </li>
+        )}
       </ul>
     </div>)
 }
@@ -179,6 +248,14 @@ type GetTodosData = {
 
 type AddTodoData = {
   createTodo: Todo
+}
+
+type DeleteTodoData = {
+  removeTodo: Pick<Todo, "id">
+}
+
+type ChangeTodoStatusData = {
+  removeTodo: Pick<Todo, "id" | "completed">
 }
 
 type Todo = {
